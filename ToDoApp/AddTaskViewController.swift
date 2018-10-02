15b16c3fa382c07e4c1bb77bc183574193e13829
陰山賢太 <kageken@kageyamakentanoMacBook-puro.local>
2018-10-02 16:24:33 +0900

    Add Task menu

---
+++ ToDoApp/AddTaskViewController.swift
@@ -0,0 +1,52 @@
+//
+//  AddTaskViewController.swift
+//  ToDoApp
+//
+//  Created by 陰山賢太 on 2018/10/02.
+//  Copyright © 2018年 Kageken. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    // MARK: - Properties
+
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var categorySegmentedControl: UISegmentedControl!
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+       
+    }
+
+    // MARK: - Actions of Buttons
+    @IBAction func categoryChosen(_ sender: Any) {
+    }
+
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func plusButtonTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
